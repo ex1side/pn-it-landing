9afec4b6f41738366f1efdc1d83f87aa2e9c921b
ex1side <ruslanprokopchuk@proton.me>
2026-04-07 02:45:33 +0300

    add sevices and schedule pages, build

--- src/main.js
+++ src/main.js
@@ -1,1 +1,59 @@
-import './style.scss'
+import './style.scss';
+import EmblaCarousel from 'embla-carousel';
+
+const containerId = document.querySelector('.embla');
+if (containerId) {
+  const options = { loop: true };
+  const viewportNode = containerId.querySelector('.embla__viewport');
+  const prevBtn = containerId.querySelector('.embla__button--prev');
+  const nextBtn = containerId.querySelector('.embla__button--next');
+  const dotsNode = document.querySelector('.embla__dots');
+
+  const emblaApi = EmblaCarousel(viewportNode, options);
+
+  const togglePrevNextBtnsState = () => {
+    if (emblaApi.canScrollPrev()) prevBtn.classList.remove('embla__button--disabled');
+    else prevBtn.classList.add('embla__button--disabled');
+
+    if (emblaApi.canScrollNext()) nextBtn.classList.remove('embla__button--disabled');
+    else nextBtn.classList.add('embla__button--disabled');
+  };
+
+  const addDotBtnsAndClickHandlers = () => {
+    dotsNode.innerHTML = emblaApi.scrollSnapList()
+      .map(() => '<button class="embla__dot" type="button"></button>')
+      .join('');
+
+    const dotNodes = Array.from(dotsNode.querySelectorAll('.embla__dot'));
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener('click', () => emblaApi.scrollTo(index), false);
+    });
+
+    return dotNodes;
+  };
+
+  let dotNodes = [];
+
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap();
+    const selected = emblaApi.selectedScrollSnap();
+    if (dotNodes[previous]) dotNodes[previous].classList.remove('embla__dot--selected');
+    if (dotNodes[selected]) dotNodes[selected].classList.add('embla__dot--selected');
+  };
+
+  prevBtn.addEventListener('click', () => emblaApi.scrollPrev(), false);
+  nextBtn.addEventListener('click', () => emblaApi.scrollNext(), false);
+
+  const initDots = () => {
+    dotNodes = addDotBtnsAndClickHandlers();
+    toggleDotBtnsActive();
+  };
+
+  initDots();
+  togglePrevNextBtnsState();
+
+  emblaApi.on('select', togglePrevNextBtnsState)
+    .on('reInit', togglePrevNextBtnsState)
+    .on('reInit', initDots)
+    .on('select', toggleDotBtnsActive);
+}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    base:'/pn-it-landing/',
+    base: '/pn-it-landing/',
 
     build: {
-        outDir:'docs',
+        outDir: 'docs',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                services: resolve(__dirname, 'services.html'),
+                schedule: resolve(__dirname, 'schedule-a-consult.html')
+            }
+        }
     }
 })
